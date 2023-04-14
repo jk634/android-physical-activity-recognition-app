552d69b011e552f573e2f25be63e1cf9f0fd7ed6
Jussi <jussi.kanto@tuni.fi>
2023-04-14 21:41:51 +0300

    Connect the accelerometer to the MainActivity class

--- app/src/main/java/fi/juka/activityrecognizer/MainActivity.kt
+++ app/src/main/java/fi/juka/activityrecognizer/MainActivity.kt
@@ -2,10 +2,23 @@ package fi.juka.activityrecognizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import fi.juka.accelerometer.Accelerometer
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var accelerometer: Accelerometer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        accelerometer = Accelerometer(this)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        accelerometer.register()
     }
 }
